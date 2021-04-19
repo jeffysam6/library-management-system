915878175d445797e46888e43646637fb779c8af
jeffysam6 <jeffysam02@gmail.com>
2021-04-19 20:20:56 +0530

    updated

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,47 +1,75 @@
 from django.contrib import admin
 
-from catalog.models import Author,Genre,Book,BookInstance
+# Register your models here.
 
+from .models import Author, Genre, Book, BookInstance, Language
 
-# admin.site.register(Book)
-# admin.site.register(Author)
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
+admin.site.register(Language)
+"""
+
+admin.site.register(Genre)
+admin.site.register(Language)
 
 
 class BooksInline(admin.TabularInline):
-	model = Book
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
 
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-	list_display = ('last_name','first_name','date_of_birth','date_of_death')
-	fields = ['first_name','last_name',('date_of_birth','date_of_death')]
-
-	inlines = [BooksInline]
-
-admin.site.register(Author,AuthorAdmin)
-
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
     model = BookInstance
 
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
-    list_filter = ('genre',)
+
+
+admin.site.register(Book, BookAdmin)
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-	list_display = ('id','book','borrower','due_back','status')
-	list_filter = ('status','due_back')
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
 
-	fieldsets = (
-		(None,{
-			'fields':('book','id')
-			}),
-		('Availability',
-			{'fields':('status','due_back','borrower')
-			}),
-		)
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,7 +1,7 @@
 """locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
 from django.contrib import admin
-from django.urls import path,include
-from django.views.generic import RedirectView
+
+# Use include() to add URLS from the catalog application and authentication system
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/',include('catalog.urls')),
-    path("",RedirectView.as_view(url='catalog/')),
+]
+
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
